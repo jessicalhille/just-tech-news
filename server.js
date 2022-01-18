c3ea5991f8973be3a2255fea805f63c170b42d2e
Jessica Hille <jessicalhille@gmail.com>
2022-01-17 20:54:00 -0600

    handlebars install, began front end dev

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+const path = require('path');
 const express = require('express');
 // collects everything from the routes
-const routes = require('./routes');
+const routes = require('./controllers/');
 // importing the connection to sequelize
 const sequelize = require('./config/connection');
+// set up handlebars.js
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
